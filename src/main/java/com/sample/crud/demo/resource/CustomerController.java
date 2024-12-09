c2511cbab88d9f2487115df97ec262a512de4e4f
VGADWAL <vgadwal7@gmail.com>
2024-12-08 20:26:33 -0500

    Added api integration tests
    Added api integration tests

--- src/main/java/com/sample/crud/demo/resource/CustomerController.java
+++ src/main/java/com/sample/crud/demo/resource/CustomerController.java
@@ -1,7 +1,10 @@
 package com.sample.crud.demo.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 
 import com.sample.crud.demo.persistence.Customer;
 import com.sample.crud.demo.persistence.CustomerRepository;
@@ -21,29 +24,43 @@ public class CustomerController {
     }
 
     @GetMapping("/customer/{id}")
-    public Customer getCustomerById(@PathVariable Long id) {
+    public Customer getCustomerById(@PathVariable Long id) throws RuntimeException{
     	 return customerRepository.findById(id).orElseThrow(() -> new RuntimeException("Customer not found"));
     }
 
     @PostMapping("/customer")
-    public Customer createCustomer(@RequestBody Customer customer) {
-    	return customerRepository.save(customer);
+    public Customer createCustomer(@RequestBody Customer customer)throws RuntimeException {
+    	try {
+    		customerRepository.save(customer);
+    	}catch(Exception e) {
+    		throw new RuntimeException("Customer not created");
+    	}
+        return customer;
     }
 
     @PutMapping("/customer/{id}")
-    public Customer updateCustomer(@PathVariable Long id, @RequestBody Customer customer) {
-    	Customer existingCustomer = getCustomerById(id);
-    	existingCustomer.setFirstName(customer.getFirstName());
-    	existingCustomer.setMiddleName(customer.getMiddleName());
-    	existingCustomer.setLastName(customer.getLastName());
-    	existingCustomer.setEmailAddress(customer.getEmailAddress());
-    	existingCustomer.setPhoneNumber(customer.getPhoneNumber());
-        return customerRepository.save(existingCustomer);
+    public Customer updateCustomer(@PathVariable Long id, @RequestBody Customer customer)throws RuntimeException{    	
+    	try {
+    		Customer existingCustomer = getCustomerById(id);
+	    	existingCustomer.setFirstName(customer.getFirstName());
+	    	existingCustomer.setMiddleName(customer.getMiddleName());
+	    	existingCustomer.setLastName(customer.getLastName());
+	    	existingCustomer.setEmailAddress(customer.getEmailAddress());
+	    	existingCustomer.setPhoneNumber(customer.getPhoneNumber());
+	    	customerRepository.save(existingCustomer);
+    	}catch(Exception e) {
+    		throw new RuntimeException("Customer not updated for id "+id);
+    	}
+        return customer;
     }
 
     @DeleteMapping("/customer/{id}")
-    public void deleteCustomer(@PathVariable Long id) {
-    	customerRepository.deleteById(id);
+    public void deleteCustomer(@PathVariable Long id)throws HttpServerErrorException {
+    	try {
+    		customerRepository.deleteById(id);
+    	}catch(Exception e) {
+    		throw new RuntimeException("Customer delete failed for id "+id);
+    	}
     }
 }
 
--- src/test/java/com/sample/crud/demo/SpringCrudJpaDemoApplicationTests.java
+++ src/test/java/com/sample/crud/demo/SpringCrudJpaDemoApplicationTests.java
@@ -1,13 +1,110 @@
 package com.sample.crud.demo;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import com.sample.crud.demo.persistence.Customer;
+
+@SpringBootTest(classes = SpringCrudJpaDemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringCrudJpaDemoApplicationTests {
 
+	@LocalServerPort
+	private int port;
+	TestRestTemplate restTemplate = new TestRestTemplate();
+	HttpHeaders headers = new HttpHeaders();
+
 	@Test
-	void contextLoads() {
+	public void testRetrieveEmptyCustomers() throws JSONException {
+		HttpEntity<String> entity = new HttpEntity<>(null, headers);
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/api/customers"),
+				HttpMethod.GET, entity, String.class);
+		String expected = "{}";
+		JSONAssert.assertEquals(expected, response.getBody(), false);
+	}
+	
+	@Test
+	public void testCreateCustomer() throws JSONException {
+		Customer customer =new Customer("Test_First","Test_Middle","Test_Last","test@email.com","test288939905");
+		HttpEntity<Customer> entity = new HttpEntity<>(new Customer("Test_First","Test_Middle","Test_Last","test@email.com","test288939905"), headers);
+		ResponseEntity<Customer> response = restTemplate.exchange(createURLWithPort("/api/customer"),
+				HttpMethod.POST, entity, Customer.class);		
+		JSONAssert.assertEquals(customer.getFirstName(),response.getBody().getFirstName(),true);
+		JSONAssert.assertEquals(customer.getLastName(),response.getBody().getLastName(),true);
+		JSONAssert.assertEquals(customer.getMiddleName(),response.getBody().getMiddleName(),true);
+		JSONAssert.assertEquals(customer.getEmailAddress(),response.getBody().getEmailAddress(),true);
+		JSONAssert.assertEquals(customer.getPhoneNumber(),response.getBody().getPhoneNumber(),true);
+		
+	}
+	
+	@Test
+	public void testGetCustomerById() throws JSONException {
+		Customer customer =new Customer("Test_First","Test_Middle","Test_Last","test@email.com","test288939905");
+		HttpEntity<Customer> entity = new HttpEntity<>(new Customer("Test_First","Test_Middle","Test_Last","test@email.com","test288939905"), headers);
+		ResponseEntity<Customer> postCustomerResponse = restTemplate.exchange(createURLWithPort("/api/customer"),
+				HttpMethod.POST, entity, Customer.class);	
+		
+		
+		Customer createdCustomer =postCustomerResponse.getBody();
+		HttpEntity<Customer> entityForPut = new HttpEntity<>(createdCustomer, headers);
+		ResponseEntity<Customer> response = restTemplate.exchange(createURLWithPort("/api/customer/"+createdCustomer.getId()),
+				HttpMethod.GET, entityForPut, Customer.class);	
+		JSONAssert.assertEquals(customer.getFirstName(),response.getBody().getFirstName(),true);
+		JSONAssert.assertEquals(customer.getLastName(),response.getBody().getLastName(),true);
+		JSONAssert.assertEquals(customer.getMiddleName(),response.getBody().getMiddleName(),true);
+		JSONAssert.assertEquals(customer.getEmailAddress(),response.getBody().getEmailAddress(),true);
+		JSONAssert.assertEquals(customer.getPhoneNumber(),response.getBody().getPhoneNumber(),true);
+		
+	}
+	
+	@Test
+	public void testPutCustomer() throws JSONException {
+		Customer customer =new Customer("Test_First","Test_Middle","Test_Last","test@email.com","test288939905");
+		HttpEntity<Customer> entity = new HttpEntity<>(new Customer("Test_First","Test_Middle","Test_Last","test@email.com","test288939905"), headers);
+		ResponseEntity<Customer> postCustomerResponse = restTemplate.exchange(createURLWithPort("/api/customer"),
+				HttpMethod.POST, entity, Customer.class);	
+		
+		
+		Customer createdCustomer =postCustomerResponse.getBody();
+		HttpEntity<Customer> entityForPut = new HttpEntity<>(createdCustomer, headers);
+		ResponseEntity<Customer> response = restTemplate.exchange(createURLWithPort("/api/customer/"+createdCustomer.getId()),
+				HttpMethod.POST, entityForPut, Customer.class);	
+		JSONAssert.assertEquals(customer.getFirstName(),response.getBody().getFirstName(),true);
+		JSONAssert.assertEquals(customer.getLastName(),response.getBody().getLastName(),true);
+		JSONAssert.assertEquals(customer.getMiddleName(),response.getBody().getMiddleName(),true);
+		JSONAssert.assertEquals(customer.getEmailAddress(),response.getBody().getEmailAddress(),true);
+		JSONAssert.assertEquals(customer.getPhoneNumber(),response.getBody().getPhoneNumber(),true);
+		
+	}
+	
+	@Test
+	public void testDeleteCustomer() throws JSONException {
+		Customer customer =new Customer("Test_First","Test_Middle","Test_Last","test@email.com","test288939905");
+		HttpEntity<Customer> entity = new HttpEntity<>(new Customer("Test_First","Test_Middle","Test_Last","test@email.com","test288939905"), headers);
+		ResponseEntity<Customer> postCustomerResponse = restTemplate.exchange(createURLWithPort("/api/customer"),
+				HttpMethod.POST, entity, Customer.class);	
+		
+		
+		Customer createdCustomer =postCustomerResponse.getBody();
+		HttpEntity<Customer> entityForDelete = new HttpEntity<>(createdCustomer, headers);
+		restTemplate.exchange(createURLWithPort("/api/customer/"+createdCustomer.getId()),
+				HttpMethod.DELETE, entityForDelete, Customer.class);
+		
+		ResponseEntity<Customer> response = restTemplate.exchange(createURLWithPort("/api/customer/"+createdCustomer.getId()),
+				HttpMethod.GET, entityForDelete, Customer.class);
+		JSONAssert.assertEquals(null,response.getBody().getFirstName(),true);
+		
+	}
+
+	private String createURLWithPort(String uri) {
+		return "http://localhost:" + port + uri;
 	}
 
 }
